546f626c19655bd567777bc4e46f2528883dcf38
Luis Madrigal <lmadrigal.111@gmail.com>
2016-11-10 11:47:57 +0800

    new models

---
+++ app/Reservation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    //
+    public function room() {
+    	return $this->belongsTo('App/Room');
+    }
+
+    public function event() {
+    	return $this->hasOne('App/Event');
+    }
+
+    public function reservee() {
+    	return $this->hasOne('App/Reservee')
+    }
+}
