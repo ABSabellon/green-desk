b0ed040a7a16eee556b998800baec2ff9fbbf9ae
Luis Madrigal <lmadrigal.111@gmail.com>
2016-11-12 22:01:49 +0800

    Update 2016_10_27_051516_create_reservation_table.php

--- database/migrations/2016_10_27_051516_create_reservation_table.php
+++ database/migrations/2016_10_27_051516_create_reservation_table.php
@@ -13,19 +13,17 @@ class CreateReservationTable extends Migration
      */
     public function up()
     {
-        Schema::create('reservation', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('reservee_id')->unsigned();
-            $table->foreign('reservee_id')->references('id')->on('reservee');
+            $table->foreign('reservee_id')->references('id')->on('reservees');
             $table->integer('room_id')->unsigned();
-            $table->foreign('room_id')->references('id')->on('room');
+            $table->foreign('room_id')->references('id')->on('rooms');
             $table->string('name', 100);
             $table->string('description', 500);
             $table->date('date');
             $table->time('time_start');
             $table->time('time_end');
-            $table->integer('event_id')->unsigned();
-            $table->foreign('event_id')->references('id')->on('event');
             $table->string('status', 10); //create table?
             $table->boolean('archived')->default(false); //create table?
             $table->timestamps();
@@ -39,6 +37,6 @@ class CreateReservationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reservation');
+        Schema::dropIfExists('reservations');
     }
 }
