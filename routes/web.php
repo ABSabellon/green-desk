8ce9911a7335fe95e6fc345e44ffa39662a1bdae
Luis Madrigal <lmadrigal.111@gmail.com>
2016-11-21 13:58:09 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,57 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('newview');
 });
+
+Route::post('/add_reservation', [
+    'uses' => 'ReservationController@postAddReservation',
+    'as' => 'add.reservation'
+]);
+
+Route::get('/get_reservations', [
+    'uses' => 'ReservationController@getReservations',
+    'as' => 'get.reservations'
+]);
+
+Route::post('/edit_reservation', [
+    'uses' => 'ReservationController@postEditReservation',
+    'as' => 'edit.reservation'
+]);
+
+Route::get('/get_rooms', [
+    'uses' => 'RoomController@getRooms',
+    'as' => 'get.rooms'
+]);
+
+Route::get('/get_rooms_by_floor', [
+    'uses' => 'RoomController@getRoomsByFloor',
+    'as' => 'get.rooms.floor'
+]);
+
+Route::get('/search_rooms', [
+    'uses' => 'SearchController@getRooms',
+    'as' => 'search.rooms'
+]);
+
+Route::get('/search_reservations', [
+    'uses' => 'SearchController@getReservations',
+    'as' => 'search.reservations'
+]);
+
+Route::get('/get_professors', [
+    'uses' => 'ReserveeController@getProfessors',
+    'as' => 'get.professors'
+]);
+
+Route::post('/add_professor', [
+    'uses' => 'ReserveeController@postAddProfessor',
+    'as' => 'add.professor'
+]);
