6bc26df5f992940c0d08db4e501bcee0df780584
Luis Madrigal <lmadrigal.111@gmail.com>
2016-12-05 20:42:34 +0800

    Create exam_reservations.js

---
+++ public/js/exam_reservations.js
@@ -0,0 +1,139 @@
+$(document).on('click', 'table .resrows', function(){
+	$('#inputWarning').hide();
+	$("table tr").css("background", "#FFF");
+	$("table tr").css("color", "#000");
+	$(this).css("background", "#00d771");
+	$(this).css("color", "#FFF");
+	resId = $(this).attr('data-id');
+	$('#schedCtrl_subject > span').html($(this).find('td.ressubj')[0].innerText);
+	$('#schedCtrl_section > span').html($(this).find('td.ressect')[0].innerText);
+	$('#schedCtrl_profName > span').html($(this).find('td.resname')[0].innerText);
+	$('#schedCtrl_profType > span').html($(this).find('td.resname').attr('data-status'));
+	$('#schedCtrl_profBase > span').html($(this).find('td.resname').attr('data-base'));
+	$('#schedCtrl_profCollege > span').html($(this).find('td.resname').attr('data-college'));
+
+	if($(this).find('td.restime')[0] != null) {
+		$('#editBtn').text('Edit');
+		var times = $(this).find('td.restime')[0].innerText.split('-');
+		$('#startTime').val(times[0]);
+		$('#endTime').val(times[1]);
+		$('#schedCtrl_room').val($(this).find('td.resroom')[0].innerText);
+
+	} else {
+		$('#editBtn').text('Create');
+		$('#startTime').val('');
+		$('#endTime').val('');
+		$('#schedCtrl_room').val('');
+	}
+});
+
+var resId;
+
+$(document).ready(function(){
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		}
+	});
+	$('#inputWarning').hide();
+	retrieveReservations(null);
+	retrieveRooms(null);
+	retrieveProfessors(null);
+});
+
+function retrieveReservations(filter) {
+	$.ajax({
+		method: 'GET',
+		url: urlGetReservations,
+		data: {filter:filter}
+	})
+	.done( function(msg) {
+		reservations = msg.reservations;
+		refreshReservations(msg.reservations);
+	});
+}
+
+function refreshReservations(reservations) {
+	$('#reservationList').empty();
+	var table = $('#schedTable > tbody');
+	for (var i = 0; i < reservations.length; i++) {
+		var reserveeName = reservations[i].reservee.first_name +' '+ reservations[i].reservee.middle_name +' '+ reservations[i].reservee.last_name;
+		var toAppend = '<tr data-id = '+reservations[i].id+' class = "resrows"><td class = "ressubj">' +reservations[i].exam.subject+ '</td><td class = "ressect">' +reservations[i].exam.section+ '</td><td class = "resname" data-status = "' +reservations[i].reservee.professor_status+ '" data-college = "' +reservations[i].reservee.professor_college+ '" data-base = "' +reservations[i].reservee.professor_base+ '">' +reserveeName+ '</td>';
+		if(reservations[i].time_start == null) {
+			toAppend = toAppend + '<td>No reservation yet</td><td></td></tr>'
+		} else {
+			toAppend = toAppend + '<td class = "restime"><time datetime="' +reservations[i].time_start+ '">' +reservations[i].time_start+ '</time>-<time datetime="' +reservations[i].time_end+ '">' +reservations[i].time_end+ '</time></td><td class = "resroom">' +reservations[i].room_no+ '</td></tr>'
+		}				
+		table.append(toAppend);
+	}
+}
+
+$('#editBtn').on('click', function() {
+	checkReservation();
+})
+
+function checkReservation() {
+	var diff = (( new Date('2016-1-1 '+ $('#endTime').val())) - new Date('2016-1-1 '+$('#startTime').val())) / 1000 / 60 / 60;
+	if(diff > 3) {
+		$('#inputWarning').text('You can only reserve for a maximum of 3 hours!');
+		$('#inputWarning').show();
+	} else if(diff < 0) {
+		$('#inputWarning').text('Your start time cannot be later than your end time!');
+		$('#inputWarning').show();
+	} else if(resId == null) {
+		$('#inputWarning').text('Please choose a reservation');
+		$('#inputWarning').show();
+	} else {
+		editReservation();
+	}
+}
+
+function editReservation() {
+	console.log('edit');
+	var startTime = $('#startTime').val();
+	var endTime = $('#endTime').val();
+	var room = $('#schedCtrl_room').val();
+
+	$.ajax({
+		method: 'POST',
+		url: urlEditReservation,
+		data: {index: resId, startTime:startTime, endTime: endTime, room:room}
+	})
+	.done( function(msg) {
+		if(msg.warning != null) {
+			$('#inputWarning').text(msg.warning);
+			$('#inputWarning').show();
+		} else {
+			$('#inputWarning').hide();
+		}
+		retrieveReservations(null);
+	});
+}
+
+var filter = $('#filterSearch').val();
+
+$('#filterSearch').on('change', function() {
+	filter = $('#filterSearch').val();
+	$('#searchRes').val('');
+	if(filter == 'prof')
+		retrieveReservations(null);
+	else
+		retrieveReservations('notnull');
+});
+
+$('#searchRes').keyup(function(event) {
+	searchResTerm = event.target.value;
+	console.log(filter);
+	searchReservations();
+});
+
+function searchReservations() {
+	$.ajax({
+		method: 'GET',
+		url: urlSearchReservations,
+		data: {term:searchResTerm, filter:filter, type:'Exam'}
+	})
+	.done( function(msg) {
+		refreshReservations(msg.reservations);
+	});
+}
