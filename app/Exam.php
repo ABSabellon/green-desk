5e41e0669def422443ba95508ee7b8e889e10453
Luis Madrigal <lmadrigal.111@gmail.com>
2016-12-07 20:10:57 +0800

    Update Exam.php

--- app/Exam.php
+++ app/Exam.php
@@ -8,6 +8,18 @@ class Exam extends Model
 {
     //
     public function reservations() {
-    	return $this->hasMany('App/Reservation');
+    	return $this->hasMany('App\Reservation');
+    }
+
+    public function subject() {
+        return $this->hasOne('App\Subject');
+    }
+
+    public function section() {
+        return $this->hasOne('App\Section');
+    }
+
+    public function takers() {
+        return $this->hasOne('App\Taker');
     }
 }
